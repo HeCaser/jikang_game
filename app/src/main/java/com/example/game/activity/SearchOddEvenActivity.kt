f714e0dacf10ee047091070fb0e341a9b95f6b08
itw_hepan <158936174@qq.com>
2019-07-11 13:58:57 +0800

    搜索奇偶数仿照ipad交互逻辑

--- app/src/main/java/com/example/game/activity/SearchOddEvenActivity.kt
+++ app/src/main/java/com/example/game/activity/SearchOddEvenActivity.kt
@@ -7,7 +7,6 @@ import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
 import android.os.Message
-import android.view.View
 import android.view.ViewTreeObserver
 import android.widget.TextView
 import androidx.core.text.isDigitsOnly
@@ -29,9 +28,11 @@ class SearchOddEvenActivity : BaseActivity() {
     companion object {
         const val TOTAL_WORD_NUMBER = 80
         const val SEARCH_WORD_NUMBER = 8
-        const val MSG_MOVE_HINT_ITEM = 1
-        const val MSG_START_MOVE = 2
-        const val MSG_TIME_COUT_DOWN = 3
+
+        const val MSG_MOVE_HINT = 1//移动指示背景(样式改变)
+        const val MSG_START_GAME = 2//开始游戏
+        const val MSG_TIME_COUNT_DOWN = 3//倒计时
+        const val MSG_GLINT = 4 //提示闪烁
 
         fun start(ctx: Context, speed: Int) {
             Intent(ctx, SearchOddEvenActivity::class.java).apply {
@@ -46,14 +47,28 @@ class SearchOddEvenActivity : BaseActivity() {
         override fun handleMessage(msg: Message?) {
             super.handleMessage(msg)
             when (msg!!.what) {
-                MSG_MOVE_HINT_ITEM -> {
-
+                MSG_MOVE_HINT -> {
+                    moveHint()
                 }
-                MSG_START_MOVE -> {
+                MSG_START_GAME -> {
                     startGame()
                 }
-                MSG_TIME_COUT_DOWN -> {
+                MSG_TIME_COUNT_DOWN -> {
                     handTimeCount()
+                }
+                MSG_GLINT -> {
+                    if (mSearPositions.size == SEARCH_WORD_NUMBER) {
+                        var pos = mSearPositions[0]
+                        var tv = flexBox[pos] as TextView
+                        var isSelect = tv.isSelected
+                        if (isSelect) {
+                            tv.setBackgroundColor(mBgColor)
+                        } else {
+                            tv.setBackgroundColor(Color.WHITE)
+                        }
+                        tv.isSelected = !isSelect
+                        this.sendEmptyMessageDelayed(MSG_GLINT, mGlintDelay)
+                    }
                 }
             }
         }
@@ -65,8 +80,8 @@ class SearchOddEvenActivity : BaseActivity() {
      */
     private fun startGame() {
         mHandler.removeCallbacksAndMessages(null)
-        mHandler.sendEmptyMessage(MSG_MOVE_HINT_ITEM)
-        mHandler.sendEmptyMessageDelayed(MSG_TIME_COUT_DOWN, mCountDownTimeDelay)
+        mHandler.sendEmptyMessageDelayed(MSG_TIME_COUNT_DOWN, mCountDownTimeDelay)
+        mHandler.sendEmptyMessageDelayed(MSG_MOVE_HINT, 0)
     }
 
     private var mRemoveCount = 0
@@ -76,8 +91,10 @@ class SearchOddEvenActivity : BaseActivity() {
     private var mTvHeight = 0F
     private var mLineMoveDelayTime = 1000L
     private var mCountDownTimeDelay = 100L
+    private var mGlintDelay = 500L
     private var mSocre = 0
     private var mTotalTime = 90
+    private var mBgColor = 0
     //是否搜索奇数
     private var isSearchOdd = true
     val padding = 4
@@ -86,6 +103,7 @@ class SearchOddEvenActivity : BaseActivity() {
     //等待选择的数据
     private var mSearPositions = arrayListOf<Int>()
     private var mStep = 0
+    private var mHintPosition = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         StatusBarUtils.setStatusBarTransparent(this)
@@ -107,15 +125,17 @@ class SearchOddEvenActivity : BaseActivity() {
     private fun initViewAndData() {
         mSpeed = intent.getIntExtra("speed", 1)
         //根据速度等级 1-10 设置移动线段的间隔 等级越高,间隔越短
-        mLineMoveDelayTime = 1500 - (mSpeed * 100).toLong()
-
+        mLineMoveDelayTime = 600 - (mSpeed * 40).toLong()
+        mBgColor = resources.getColor(R.color.color_ff8e00)
         //计算pb的最大值, 倒计时共90s
         mTotalTime = 900 * mCountDownTimeDelay.toInt()
         progressBar.max = mTotalTime
-        progressBar.progress=mTotalTime
+        progressBar.progress = mTotalTime
 
         setCenterTitle("济康-奇偶数")
-        circleStepView.setStep(SEARCH_WORD_NUMBER * 3)
+
+        //设置步进总数,根据速度设置
+        circleStepView.setStep(mSpeed * 8 + 20,0)
 
         initGameView()
 
@@ -131,6 +151,8 @@ class SearchOddEvenActivity : BaseActivity() {
         } else {
             tvSearchWhat.text = "找到偶数"
         }
+        mHintPosition = 0
+
         //添加view给flexbox
         flexBox.removeAllViews()
         for (num in 0..TOTAL_WORD_NUMBER) {
@@ -149,11 +171,11 @@ class SearchOddEvenActivity : BaseActivity() {
 
         //随机放入待选择的数字
         mSearPositions.clear()
-        for (num in 0..SEARCH_WORD_NUMBER) {
+        for (num in 0 until SEARCH_WORD_NUMBER) {
             val random = generateNumber(isSearchOdd)
             var position = Random.nextInt(0, TOTAL_WORD_NUMBER)
             while (mSearPositions.contains(position)) {
-                position = Random.nextInt(0, SEARCH_WORD_NUMBER)
+                position = Random.nextInt(0, TOTAL_WORD_NUMBER)
             }
             mSearPositions.add(position)
 
@@ -163,6 +185,9 @@ class SearchOddEvenActivity : BaseActivity() {
                 handleWordClick(position)
             }
         }
+
+        //开启闪烁提示
+        mHandler.sendEmptyMessageDelayed(MSG_GLINT, mGlintDelay)
     }
 
     /**
@@ -173,17 +198,24 @@ class SearchOddEvenActivity : BaseActivity() {
         if (!mSearPositions.contains(pos)) return
         mSocre += 10
         tvScore.text = "$mSocre"
+        if (mSearPositions.size == SEARCH_WORD_NUMBER) {
+            //第一次点击正确,开始游戏
+            var pos = mSearPositions[0]
+            flexBox[pos].setBackgroundColor(Color.WHITE)
+            mHandler.sendEmptyMessage(MSG_START_GAME)
+        }
         val tv = flexBox[pos] as TextView
-        tv.setBackgroundColor(Color.RED)
+        tv.setBackgroundColor(mBgColor)
         tv.setTextColor(Color.WHITE)
+
         circleStepView.setStep(++mStep)
         mSearPositions.remove(pos)
+
+
         if (mSearPositions.isEmpty()) {
             //全部找完
             mRemoveCount = 0
             initGameView()
-            mHandler.removeMessages(MSG_MOVE_HINT_ITEM)
-            mHandler.sendEmptyMessageDelayed(MSG_MOVE_HINT_ITEM, 50L)
         }
     }
 
@@ -196,7 +228,22 @@ class SearchOddEvenActivity : BaseActivity() {
             finisGame()
         }
         progressBar.progress = mTotalTime
-        mHandler.sendEmptyMessageDelayed(MSG_TIME_COUT_DOWN, mCountDownTimeDelay)
+        mHandler.sendEmptyMessageDelayed(MSG_TIME_COUNT_DOWN, mCountDownTimeDelay)
+    }
+
+    /**
+     * 提示状态的移动,根据传入的速度来决定移动快慢
+     */
+    private fun moveHint() {
+        if (mHintPosition >= TOTAL_WORD_NUMBER) return
+        val tv = flexBox[mHintPosition] as TextView
+        tv.setShadowLayer(2.0f, 1.0f, 1.0f, resources.getColor(R.color.colorPrimary))
+        if (mHintPosition != 0) {
+            val tvBefore = flexBox[mHintPosition - 1] as TextView
+            tvBefore.setShadowLayer(.0f, .0f, .0f, Color.WHITE)
+        }
+        mHintPosition++
+        mHandler.sendEmptyMessageDelayed(MSG_MOVE_HINT, mLineMoveDelayTime)
     }
 
     /**
@@ -212,11 +259,6 @@ class SearchOddEvenActivity : BaseActivity() {
     private fun getScore(): Int {
         val text = tvScore.text.toString()
         return if (text.isDigitsOnly()) text.toInt() else 0
-    }
-
-    override fun onResume() {
-        super.onResume()
-        mHandler.sendEmptyMessageDelayed(MSG_START_MOVE, 500)
     }
 
     /**
