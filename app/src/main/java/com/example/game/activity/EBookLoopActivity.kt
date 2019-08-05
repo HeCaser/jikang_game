c5e7ab30a564742c94f8c09ae986249d50369175
itw_hepan <158936174@qq.com>
2019-08-05 19:33:41 +0800

    ebook循环提交

--- app/src/main/java/com/example/game/activity/EBookLoopActivity.kt
+++ app/src/main/java/com/example/game/activity/EBookLoopActivity.kt
@@ -6,12 +6,15 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Message
-import android.util.Log
+import android.text.TextUtils
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.game.R
 import com.example.game.constant.BOOK_ZHONGQIUJIE
 import com.example.game.database.AppDatabase
+import com.example.game.database.ArticleLine
+import com.example.game.utils.SaveSpData
+import com.example.game.utils.ScreenUtils
 import com.example.game.utils.StatusBarUtils
 import com.example.game.viewmodel.ArticleLineViewModel
 import com.example.game.viewmodel.ArticleLineViewModelFactory
@@ -26,7 +29,7 @@ class EBookLoopActivity : BaseActivity() {
 
     companion object {
         const val MSG_START_GAME = 1
-        const val MSG_MOVE_LINE = 2
+        const val MSG_MOVE_CIRCLE = 2
         fun start(ctx: Context) {
             Intent(ctx, EBookLoopActivity::class.java).apply {
                 ctx.startActivity(this)
@@ -35,17 +38,31 @@ class EBookLoopActivity : BaseActivity() {
     }
 
     private var mCircleWidth = 0
-    private var mStartLine =0
+    private var mMindWidth=0
+    private var mMaxWidth=0
+    private var mStartLine = 0
+    private var mStep = 10
+    private var mMoveCircleDelay = 100L
     val TAG = EBookLoopActivity::class.java.simpleName
+    //文章的行集合
+    private var mLines = listOf<ArticleLine>()
+    private var mContents = "" //文本
+
     private var mHandler = @SuppressLint("HandlerLeak")
     object : Handler() {
         override fun handleMessage(msg: Message?) {
             super.handleMessage(msg)
             when (msg!!.what) {
                 MSG_START_GAME -> {
-                    initShowView()
+                    startGame()
                 }
-                MSG_MOVE_LINE -> {
+                MSG_MOVE_CIRCLE -> {
+                    mCircleWidth+=4
+                    if (mCircleWidth>=mMaxWidth){
+                        mCircleWidth= mMindWidth
+                    }
+                    circleView.updateWidth(mCircleWidth)
+                    this.sendEmptyMessageDelayed(MSG_MOVE_CIRCLE,mMoveCircleDelay)
                 }
             }
         }
@@ -68,6 +85,13 @@ class EBookLoopActivity : BaseActivity() {
 
     private fun initViewAndData() {
         mCircleWidth = circleView.getmWidth()
+        mMindWidth = circleView.getmWidth()
+        mMaxWidth = (ScreenUtils.getScreenSize(this).x*0.8).toInt()
+
+        if (TextUtils.isEmpty(SaveSpData.newInstance(this).getCommomStringData(BOOK_ZHONGQIUJIE))) {
+            articleLineViewModel.saveBook(this, BOOK_ZHONGQIUJIE)
+        }
+        mHandler.sendEmptyMessageDelayed(MSG_START_GAME, 500)
     }
 
     private fun initListener() {
@@ -81,27 +105,55 @@ class EBookLoopActivity : BaseActivity() {
         }
 
         articleLineViewModel.lines.observe(this, Observer {
-            Log.e(TAG,"${it.size}")
+            handleData(it)
         })
     }
 
 
-
-    override fun onResume() {
-        super.onResume()
-        mHandler.sendEmptyMessageDelayed(MSG_START_GAME, 100)
+    /**
+     * 开始游戏就是去数据库里取数据
+     */
+    private fun startGame() {
+        articleLineViewModel.getLineFromIndex(mStartLine, mStep, BOOK_ZHONGQIUJIE)
     }
 
-    private fun initShowView() {
+    /**
+     * 处理从数据库返回的数据
+     */
+    private fun handleData(lines: List<ArticleLine>) {
+        if (lines.isEmpty() && mStartLine == 0) {
+            //数据还没准备好,延时再次获取
+            mHandler.sendEmptyMessageDelayed(MSG_START_GAME, 500)
+            return
+        }
+
+        if (lines.isEmpty()) {
+            finisGame()
+            return
+        }
+        mLines = lines
+        if (mStartLine==mStep){
+            //首次获取数据,开始动画,显示文字
+            mHandler.sendEmptyMessageDelayed(MSG_MOVE_CIRCLE,mMoveCircleDelay)
+        }
+        getData()
     }
 
+
+    private fun changeLinesToString(){
+        val builder = StringBuilder()
+        mLines.forEach {
+            builder.append(it.content)
+        }
+        mContents = builder.toString()
+    }
 
     /**
      * 结束游戏
      */
     private fun finisGame() {
         mHandler.removeCallbacksAndMessages(null)
-        mHandler.sendEmptyMessage(MSG_MOVE_LINE)
+        mHandler.sendEmptyMessage(MSG_MOVE_CIRCLE)
     }
 
     override fun onDestroy() {
@@ -110,7 +162,7 @@ class EBookLoopActivity : BaseActivity() {
     }
 
     fun getData() {
-        articleLineViewModel.getLineFromIndex(mStartLine,10, BOOK_ZHONGQIUJIE)
-        mStartLine+=10
+        articleLineViewModel.getLineFromIndex(mStartLine, mStep, BOOK_ZHONGQIUJIE)
+        mStartLine += mStep
     }
 }
