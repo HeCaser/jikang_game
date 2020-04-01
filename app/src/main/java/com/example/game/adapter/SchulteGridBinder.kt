bb376648101a5b18731422df0774d379510b128c
hepan <he_pann@163.com>
2020-04-01 14:12:08 +0800

    舒尔特字体大小适配

--- app/src/main/java/com/example/game/adapter/SchulteGridBinder.kt
+++ app/src/main/java/com/example/game/adapter/SchulteGridBinder.kt
@@ -1,6 +1,7 @@
 package com.example.game.adapter
 
 import android.graphics.Color
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,8 +9,10 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.game.R
 import com.example.game.bean.NumberBean
 import com.example.game.util.dp2px
+import com.example.game.util.screenWidth
 import kotlinx.android.synthetic.main.schulte_attention_item.view.*
 import me.drakeet.multitype.ItemViewBinder
+import kotlin.math.sqrt
 
 class SchulteGridBinder(var span: Int, var callBack: (phone: Int) -> Unit) :
         ItemViewBinder<NumberBean, SchulteGridBinder.ViewHolder>() {
@@ -28,8 +31,15 @@ class SchulteGridBinder(var span: Int, var callBack: (phone: Int) -> Unit) :
         fun setData() {
             with(itemView) {
                 tvNumber.text = "${mData.number}"
-                var size = context.dp2px(50f - (span - 2) * 3)
-                tvNumber.textSize = size.toFloat()
+//                var size = context.dp2px(50f - (span - 2) * 3)
+//                tvNumber.textSize = size.toFloat()
+
+                var number =adapterItems.size
+                var screenWidth = screenWidth
+                screenWidth-=span*context.dp2px(10f)
+                var size2 = screenWidth/ sqrt(number.toDouble())
+                tvNumber.setTextSize(TypedValue.COMPLEX_UNIT_PX,size2.toFloat())
+
                 if (mData.isSelected) {
                     tvNumber.setBackgroundColor(Color.WHITE)
                     tvNumber.setTextColor(context.resources.getColor(R.color.colorPrimary))
