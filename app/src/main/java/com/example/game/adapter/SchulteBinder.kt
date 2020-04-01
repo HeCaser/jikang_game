255e41b52b0bd92d16d1dc67c2a526a2cdc5c3cb
hepan <he_pann@163.com>
2020-04-01 14:10:22 +0800

    舒尔特方格,舒尔特,舒尔特注意力 文字大小适配

--- app/src/main/java/com/example/game/adapter/SchulteBinder.kt
+++ app/src/main/java/com/example/game/adapter/SchulteBinder.kt
@@ -1,13 +1,17 @@
 package com.example.game.adapter
 
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.game.R
 import com.example.game.bean.SchulteItemBean
+import com.example.game.util.dp2px
+import com.example.game.util.screenWidth
 import kotlinx.android.synthetic.main.schulte_item.view.*
 import me.drakeet.multitype.ItemViewBinder
+import kotlin.math.sqrt
 
 class SchulteBinder(var callBack: (phone: Int) -> Unit) :
         ItemViewBinder<SchulteItemBean, SchulteBinder.ViewHolder>() {
@@ -26,6 +30,14 @@ class SchulteBinder(var callBack: (phone: Int) -> Unit) :
         fun setData() {
             with(itemView) {
                 tvNumber.text = "${mData.showText}"
+
+                //根据屏幕宽度,item间隔,每行item数量确定文字尺寸
+                var number =adapterItems.size
+                var screenWidth = screenWidth
+                screenWidth-=context.dp2px(60f) //减去rv的padding
+                var size2 = screenWidth/ sqrt(number.toDouble())
+                tvNumber.setTextSize(TypedValue.COMPLEX_UNIT_PX,size2.toFloat())
+
                 itemView.setOnClickListener {
                     callBack(adapterPosition)
                 }
